c13c9b39352b9b5490bef30040ef1c8e573d60f7
unknown <enzohn2016@gmail.com>
2024-10-17 21:47:57 -0300

    meu primeiro commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+console.log("Olá");
+
+//git init -> inicializa o git
+//git remote add origin -> cria um link entre o repositorio local e o repositorio no provedor de repositorios
+//git add -> adiciona os arquivos no repositorio
+//git status -> ele lista os arquivos (adicionados, modificados)
