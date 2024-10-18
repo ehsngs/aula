530effd5f3bfc61acf41616d3a9eaae4d70fd185
ehsngs <enzohn2016@gmail.com>
2024-10-17 21:51:08 -0300

    meu primeiro commit

--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@ console.log("Olá");
 //git remote add origin -> cria um link entre o repositorio local e o repositorio no provedor de repositorios
 //git add -> adiciona os arquivos no repositorio
 //git status -> ele lista os arquivos (adicionados, modificados)
+//git commit
