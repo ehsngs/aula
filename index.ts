180ed167fbfb49dda5f29291ee3f96aaf9038bfc
ehsngs <enzohn2016@gmail.com>
2024-10-17 22:04:42 -0300

    Segundo comit

--- index.ts
+++ index.ts
@@ -4,4 +4,5 @@ console.log("Olá");
 //git remote add origin -> cria um link entre o repositorio local e o repositorio no provedor de repositorios
 //git add -> adiciona os arquivos no repositorio
 //git status -> ele lista os arquivos (adicionados, modificados)
-//git commit
+//git commit ->log
+console.log();
